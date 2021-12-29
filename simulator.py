700c00b2cee382360d6b80c3934474a150e0d58b
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-12-29 14:41:50 +0900

    change kalmangain const to function of t

--- simulator.py
+++ simulator.py
@@ -2,22 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 initial_state = np.array([0, 0])
-KalmanGain = np.array([[0.1, 0  ],
-                       [0  , 0.2]])
 
 #config
 XSysVariance = 1
 VSysVariance = 1
-XSensVariance = 200
-VSensVariance = 5
+XSensVariance = 500
+VSensVariance = 50
 timecount = 100
 dt = 0.1
 
-input_value = [0.5]*int(timecount/dt)                   # システムへの入力u(t) 今回は等加速度運動とした
-true_position = [np.array([0,0])]*int(timecount/dt)     # システムの真の状態 [x^true, v^true] 以下，各状態は[x,v]の形
-estimate_position = [np.array([0,0])]*int(timecount/dt) # カルマンフィルタによって推定されるシステムの状態
-odometory = [np.array([0,0])]*int(timecount/dt)         # システムの入力から推定されるシステムの状態
-observation = [np.array([0,0])]*int(timecount/dt)       # センサーの値から(観測，推定される)システムの状態
+input_value = [2]*int(timecount/dt)                               # システムへの入力u(t) 今回は等加速度運動とした
+true_position = [np.array([0,0])]*int(timecount/dt)               # システムの真の状態 [x^true, v^true] 以下，各状態は[x,v]の形
+estimate_position = [np.array([0,0])]*int(timecount/dt)           # カルマンフィルタによって推定されるシステムの状態
+odometory = [np.array([0,0])]*int(timecount/dt)                   # システムの入力から推定されるシステムの状態
+observation = [np.array([0,0])]*int(timecount/dt)                 # センサーの値から(観測，推定される)システムの状態
+kalmangain = [np.array([[0.01, 0],[0, 0.02]])]*int(timecount/dt) #カルマンゲイン
 
 #calc true_position
 for t in range(int(timecount/dt)):
@@ -35,15 +34,15 @@ for t in range(int(timecount/dt)):
         continue
     odometory[t] = np.array([[1,dt],[0,1]])@estimate_position[t-1] \
                    + np.array([0.5*dt*dt,dt])*input_value[t]
-    observation[t] = np.array([true_position[t][0]+np.random.normal(0,XSensVariance), true_position[t][1]+np.random.normal(0,VSensVariance)])
-    estimate_position[t] = (np.eye(2) - KalmanGain)@odometory[t] + KalmanGain@observation[t]
+    observation[t] = true_position[t] + np.array([np.random.normal(0,XSensVariance), np.random.normal(0,VSensVariance)])
+    estimate_position[t] = (np.eye(2) - kalmangain[t])@odometory[t] + kalmangain[t]@observation[t]
 
 fig = plt.figure()
 ax_x = fig.add_subplot(2,1,1,xlabel='t',ylabel='x')
 ax_v = fig.add_subplot(2,1,2,xlabel='t',ylabel='v')
 
-#ax_x.plot(range(0,timecount,dt), [i[0] for i in odometory], label='odometory')
-#ax_v.plot(range(0,timecount,dt), [i[1] for i in odometory], label='odometory')
+#ax_x.plot(np.arange(0, timecount, dt), [i[0] for i in odometory], label='odometory')
+#ax_v.plot(np.arange(0, timecount, dt), [i[1] for i in odometory], label='odometory')
 ax_x.plot(np.arange(0, timecount, dt), [i[0] for i in observation], label='observation')
 ax_v.plot(np.arange(0, timecount, dt), [i[1] for i in observation], label='observation')
 ax_x.plot(np.arange(0, timecount, dt), [i[0] for i in true_position], label='true_position')
